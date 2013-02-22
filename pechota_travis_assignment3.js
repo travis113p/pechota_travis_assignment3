88ed13474e720fa52942ac95831794e6ebcc6fc1
Travis Pechota <travis113p@13-30-109.client.wireless.msu.edu>
2013-02-21 19:38:04 -0500

    first code. boolean.  having trouble already.

--- pechota_travis_assignment3.js
+++ pechota_travis_assignment3.js
@@ -1,1 +1,34 @@
-alert("JavaScript works!");
+//variables
+/*var day = "Thursday";
+	aisles = ["Soups", "Juices", "Candy"];
+	cartsPerAisle = 4;
+	longDayProclaim = "It's going to be a long day.";
+	shortDayProclaim = "It should be a quick day.";
+	additionalAisle1 = "Chips";
+	additionalAisle2 = "Coffee";*/
+
+var cartsPerAisle = 5;
+	whatAisles = ["soup", 
+				"paper"];
+	extraAisle = 1;
+	longNight = true;
+	iQuit = "Bill, I quit!";
+	soups = {"Progresso":["Chunky Potato", "Chicken Noodle"],
+			"Campbells":["Clam Chowder", "Beef Barley"]};
+
+//boolean
+var shortNight = function (aisles) {
+	if (aisles <= whatAisles.length) {
+		shortNight = true
+	} else {
+		shortNight = false
+	}
+	return shortNight
+}
+
+
+
+//calls
+var earlyExit = shortNight(1)
+console.log("It is " + earlyExit + " that I will leave early tonight.")
+
