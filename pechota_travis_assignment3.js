a821bf485a232d68c94470f77f3ed1a0bb867dcb
Travis Pechota <travis113p@13-30-109.client.wireless.msu.edu>
2013-02-21 20:11:41 -0500

    making progress i think.  added array, number, object.

--- pechota_travis_assignment3.js
+++ pechota_travis_assignment3.js
@@ -7,14 +7,17 @@
 	additionalAisle1 = "Chips";
 	additionalAisle2 = "Coffee";*/
 
-/*var cartsPerAisle = 5;
+var cartsPerAisle = 5;
 	whatAisles = ["soup", 
 				"paper"];
 	extraAisle = 1;
 	longNight = true;
 	iQuit = "Bill, I quit!";
-	soups = {"Progresso":["Chunky Potato", "Chicken Noodle"],
-			"Campbells":["Clam Chowder", "Beef Barley"]};
+	soups = {
+			"Progresso":["Chunky Potato", "Chicken Noodle"],
+			"Campbells":["Clam Chowder", "Beef Barley"],
+			"Meijer":false
+			};
 
 //boolean
 var shortNight = function (aisles) {
@@ -26,11 +29,21 @@ var shortNight = function (aisles) {
 	return shortNight
 }
 
+//number
+var aisleNum = function(number) {
+	while (cartsPerAisle <= number) {
+		cartsPerAisle = cartsPerAisle + 1;
+	}; console.log("I now have " + cartsPerAisle + " carts for each aisle.  I probably won't finish on time.");
+	return cartsPerAisle
+}	
 
-
-//calls
-var earlyExit = shortNight(1)
-console.log("It is " + earlyExit + " that I will leave early tonight.")*/
+//array
+var aislesDone = function(aisleName, number) {
+	for (var done = number; done < whatAisles.length; done++) {
+		console.log("I have finished " + whatAisles[done] + ".");
+	};
+	return aisleName + number;
+};
 
 //object with accessor
 var aisle = {
@@ -42,8 +55,17 @@ var aisle = {
 	}
 }
 
-//object with mutator
-
 //calls
+var carts = aisleNum(5);
+console.log("\"Bill, I don't need anymore carts.  I'll be here until 11.\"");
+
+var earlyExit = shortNight(1)
+console.log("It is " + earlyExit + " that I will leave early tonight.")
+
+var array = aislesDone(whatAisles[0], 0)
+console.log("Since " + whatAisles[0]  + " and " + whatAisles[1] + " are all done, and other stockers are finishing, they come to help me finish by 6:30, not 11.");
+
+
 console.log(aisle.timePerAisle());
+
 
